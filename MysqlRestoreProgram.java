51372c59f66344ab63ca84c7be4f4f8bde13eaa4
Thiwanka Kasun <thiwankamax@gmail.com>
2018-02-22 19:45:36 +0530

    Update MysqlRestoreProgram.java

--- MysqlRestoreProgram.java
+++ MysqlRestoreProgram.java
@@ -16,37 +16,37 @@ public class MysqlRestoreProgram {
 	
 	public static void restoreDatabase(String mysqlExe,String dbUserName, String dbPassword, String source) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-        String[] executeCmd = new String[]{mysqlExe, "--user=" + dbUserName, "--password=" + dbPassword, "-e", "source " + source};
+        	String[] executeCmd = new String[]{mysqlExe, "--user=" + dbUserName, "--password=" + dbPassword, "-e", "source " + source};
  
-        Process runtimeProcess;
-        try {
-	        System.out.println("Processing.. "+ "STARTED " +sdf.format(new Date()));
-	        Date sDate = new Date();
-	        
-            runtimeProcess = Runtime.getRuntime().exec(executeCmd);
-            int processComplete = 0;
-			try {
-				processComplete = runtimeProcess.waitFor();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-            
-            System.out.println("Processing.. "+ "END " +sdf.format(new Date()));
-	        Date eDate = new Date();
-	        long duration =  eDate.getTime() - sDate.getTime();
-	        int seconds=(int) ((duration/1000)%60);
-	        long minutes=((duration-seconds)/1000)/60;
-	        System.err.println("TOTAL TIME : " + minutes +" minutes :: ");
-	        System.err.print(seconds +" seconds :: ");
-	        System.err.print(duration +" milliseconds");
-	        
-            if (processComplete == 0) {
-                System.out.println("Backup restored successfully with " + source);
-            } else {
-            	System.out.println("Could not restore the backup " + source);
-            }
-        } catch (Exception ex) {
-        	ex.printStackTrace();
-        }
-    }
+        	Process runtimeProcess;
+		try {
+		    System.out.println("Processing.. "+ "STARTED " +sdf.format(new Date()));
+		    Date sDate = new Date();
+
+		    runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+		    int processComplete = 0;
+		    try {
+			processComplete = runtimeProcess.waitFor();
+		    } catch (Exception e) {
+			e.printStackTrace();
+		    }
+
+		    System.out.println("Processing.. "+ "END " +sdf.format(new Date()));
+		    Date eDate = new Date();
+		    long duration =  eDate.getTime() - sDate.getTime();
+		    int seconds=(int) ((duration/1000)%60);
+		    long minutes=((duration-seconds)/1000)/60;
+		    System.err.println("TOTAL TIME : " + minutes +" minutes :: ");
+		    System.err.print(seconds +" seconds :: ");
+		    System.err.print(duration +" milliseconds");
+
+		    if (processComplete == 0) {
+			System.out.println("Backup restored successfully with " + source);
+		    } else {
+			System.out.println("Could not restore the backup " + source);
+		    }
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+    	}
 }
